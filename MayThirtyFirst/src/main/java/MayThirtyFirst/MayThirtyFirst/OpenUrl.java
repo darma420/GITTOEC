6dafdb1dc4d51362e1c1b68fff0bc9f224b0b5ea
darma420 <dharma.venkatrao378@gmail.com>
2023-06-12 18:20:53 +0530

    First push...

---
+++ MayThirtyFirst/src/main/java/MayThirtyFirst/MayThirtyFirst/OpenUrl.java
@@ -0,0 +1,20 @@
+package MayThirtyFirst.MayThirtyFirst;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class OpenUrl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.co.in/");
+		Thread.sleep(5000);
+		//driver.close();	}
+
+}
+}
